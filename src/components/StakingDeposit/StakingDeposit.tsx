5d4cd298d819936fb08be0d8b5ca68f3d004482b
david-blox <david@blox.io>
2020-12-09 18:26:28 +0200

    Icon and Modal components

--- src/components/StakingDeposit/StakingDeposit.tsx
+++ src/components/StakingDeposit/StakingDeposit.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import Metamask from '../../service/Metamask';
+
+const metamask = new Metamask();
 
 const Wrapper = styled.div`
-
+  width:100%;
+  height:100%;
 `;
 
-const StakingDeposit = () => (
-  <Wrapper>
-    StakingDeposit
-  </Wrapper>
-);
+const StakingDeposit = () => {
+  useEffect(() => { console.log('metamask', metamask.isExist()); },[]);
+  return (
+    <Wrapper>
+      Staking Deposit
+    </Wrapper>
+  );
+}
 
 export default StakingDeposit;
