22064d278843347a06f7bc0b18981945255b5d44
guy muroch <guym@blox.io>
2021-08-24 11:06:01 +0300

    fixing style

--- src/components/StakingDeposit/components/Wrapper.tsx
+++ src/components/StakingDeposit/components/Wrapper.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   width:100%;
+  height: 100vh;
   padding:45px 115px 45px 115px;
   position:relative;
   background-color:${({theme}) => theme.gray100};
